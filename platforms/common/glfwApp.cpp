fc7a324d60bb617b7b8b5f5834dc58acee20bac5
Hannes Janetzek <hannes.janetzek@gmail.com>
2018-12-18 20:47:53 +0100

    Make SceneOptions public and add back blocking sceneLoad methods
    - I would still prefer to let the user create this wait-on-lock
      by themselves when needed. Even though mixing async and blocking
      sceneload calls should be safer now.

--- platforms/common/glfwApp.cpp
+++ platforms/common/glfwApp.cpp
@@ -61,6 +61,7 @@ double last_y_velocity = 0.0;
 
 bool wireframe_mode = false;
 bool show_gui = true;
+bool load_async = true;
 
 Tangram::MarkerID marker = 0;
 Tangram::MarkerID poiMarker = 0;
@@ -82,10 +83,18 @@ void loadSceneFile(bool setPosition = false, std::vector<SceneUpdate> updates = 
         if (!found) { sceneUpdates.push_back(update); }
     }
 
-    if (!sceneYaml.empty()) {
-        map->loadSceneYamlAsync(sceneYaml, sceneFile, setPosition, sceneUpdates);
+    if (load_async) {
+        if (!sceneYaml.empty()) {
+            map->loadSceneYamlAsync(sceneYaml, sceneFile, setPosition, sceneUpdates);
+        } else {
+            map->loadSceneAsync(sceneFile, setPosition, sceneUpdates);
+        }
     } else {
-        map->loadSceneAsync(sceneFile, setPosition, sceneUpdates);
+        if (!sceneYaml.empty()) {
+            map->loadSceneYaml(sceneYaml, sceneFile, setPosition, sceneUpdates);
+        } else {
+            map->loadScene(sceneFile, setPosition, sceneUpdates);
+        }
     }
 }
 
@@ -415,6 +424,10 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 
     if (action == GLFW_PRESS) {
         switch (key) {
+            case GLFW_KEY_A:
+                load_async = !load_async;
+                LOG("Toggle async load: %d", load_async);
+                break;
             case GLFW_KEY_D:
                 show_gui = !show_gui;
                 break;
